48e42968d1bd2f0617f13ad14d834675162c7e50
Louis Escher <louisescher@proton.me>
2026-04-06 16:15:44 +0200

    Update subscribe_events_handler.rs

--- src/xrpc/social/colibri/sync/subscribe_events_handler.rs
+++ src/xrpc/social/colibri/sync/subscribe_events_handler.rs
@@ -4,7 +4,7 @@ use rocket_ws::{Stream, WebSocket};
 #[get("/xrpc/social.colibri.sync.subscribeEvents")]
 /// Exposes a WebSocket connection that allows clients to receive events and send messages from and to the AppView.
 pub async fn subscribe_events(ws: WebSocket) -> Stream!['static] {
-    // TODO: This is a placeholder implementation. The frontend needs to switch to a client-based OAuth solution
+    // TODO: This is a stub implementation. The frontend needs to switch to a client-based OAuth solution
     // so this can start to work.
     ws.stream(|io| io)
 }
